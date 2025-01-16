8bd8878404da893188cec4200149382c1613005f
Tetroit <shichinov.ivan@gmail.com>
2025-01-17 00:19:41 +0100

    Absolute cinema

--- Assets/Ivan/ChipManager.cs
+++ Assets/Ivan/ChipManager.cs
@@ -23,21 +23,40 @@ public class ChipManager : MonoBehaviour
     [SerializeField]
     public Vector2 padding = new Vector2 (10, 10);
 
-
+    int currentShiftID = -1;
     Grid inventoryGrid;
 
     [SerializeField]
     RectTransform equationArea;
 
-    List<Chip> chipListInventory = new List<Chip>();
-    List<Chip> chipListEquation = new List<Chip>();
+    class LayoutInfo
+    {
+        public int id;
+        public bool isShifted;
+
+        public LayoutInfo(int id, bool isShifted = false) 
+        {
+            this.id = id;
+            this.isShifted = isShifted;
+        }
+    }
+
+    Dictionary<GameObject, LayoutInfo> inventoryItems = new Dictionary<GameObject, LayoutInfo>();
+
+
+
+    [Space(30)]
+    [Header("So this option enables dragging... Like... \n\nImagine that you are always dragging an item okay?\n As if you are about to place it into the grid")]
+    [Space(20)]
+    public bool dragging = false;
+
     public IEnumerable GetInventoryChips()
     {
-        return chipListInventory;
+        return null;
     }
     public IEnumerable GetEquationChips()
     {
-        return chipListInventory;
+        return null;
     }
 
     public Vector2 GetNextFreeGridPosition()
@@ -56,10 +75,30 @@ public class ChipManager : MonoBehaviour
     }
     private void Start()
     {
-        chipListInventory = inventoryArea.GetComponentsInChildren<Chip>().ToList();
+        for (int i = 0; i < inventoryGrid.transform.childCount; i++)
+        {
+            Transform child = inventoryGrid.transform.GetChild(i);
+
+            if (!inventoryItems.ContainsKey(child.gameObject))
+            {
+                inventoryItems.Add(child.gameObject, new LayoutInfo(i));
+            }
+        }
     }
     private void Update()
     {
+        if (dragging)
+        {
+            Vector2 cursorPos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle
+                (inventoryGrid.GetComponent<RectTransform>(), Input.mousePosition, null, out cursorPos);
+
+            int readShiftID = GetIDToMove(cursorPos);
+            Debug.Log(readShiftID);
+
+            if (readShiftID != currentShiftID)
+                ShiftInventoryItems(readShiftID);
+        }
 
     }
     [ExecuteAlways]
@@ -68,19 +107,15 @@ public class ChipManager : MonoBehaviour
 
         Gizmos.color = Color.yellow;
 
-        Vector2 cursorPos;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle
-            (inventoryGrid.GetComponent<RectTransform>(), Input.mousePosition, null, out cursorPos);
-
-        //Debug.Log(GetIDToMove(cursorPos));
-        Gizmos.DrawSphere(cursorPos, 1);
-        Vector3 pos = inventoryGrid.CellToLocal(inventoryGrid.LocalToCell(cursorPos));
-        Gizmos.DrawWireCube(inventoryGrid.transform.TransformPoint(pos), Vector3.Scale(inventoryGrid.transform.lossyScale, inventoryGrid.cellSize));
 
     }
     public Vector3Int GetGridPosition(int i)
     {
         return new Vector3Int(i % inventoryRows, - i / inventoryRows, 0);
+    }
+    public Vector3 GetLocalCellPosition(int i)
+    {
+        return inventoryGrid.CellToLocal(GetGridPosition(i));
     }
     public int GetGridID(Vector3Int pos)
     {
@@ -90,13 +125,13 @@ public class ChipManager : MonoBehaviour
     }
     public int GetIDToMove(Vector3 pos)
     {
+        pos.x += 0.5f * inventoryGrid.cellSize.x;
         pos.y += 0.5f * inventoryGrid.cellSize.y;
         Vector3Int gridPos = inventoryGrid.LocalToCell(pos);
-        gridPos.x++;
         int ID = GetGridID(gridPos);
         return ID;
     }
-    [ContextMenu("arrange")]
+    [ContextMenu("Arrange")]
     public void ArrangeInventoryChildren()
     {
         for (int i = 0; i < inventoryGrid.transform.childCount; i++)
@@ -109,7 +144,54 @@ public class ChipManager : MonoBehaviour
     public void AddToInventory(GameObject obj)
     {
         int id = inventoryArea.transform.childCount;
+        AddToInventory(obj, id);
+    }
+    public void AddToInventory(GameObject obj, int id)
+    {
         obj.transform.parent = inventoryArea.transform;
         obj.transform.localPosition = inventoryGrid.CellToLocal(GetGridPosition(id));
+        if (!inventoryItems.ContainsKey(obj))
+        {
+            inventoryItems.Add(obj, new LayoutInfo(id));
+        }
+    }
+    public void ShiftInventoryItems(int id)
+    {
+        currentShiftID = id;
+        foreach (GameObject go in inventoryItems.Keys)
+        {
+            LayoutInfo info = inventoryItems[go];
+            //we should shift forward
+            if (!info.isShifted && id > -1 && info.id >= id)
+            {
+                info.isShifted = true;
+                Vector3 targetPos = GetLocalCellPosition(info.id + 1);
+                //go.transform.localPosition = targetPos;
+                StartCoroutine(ShiftAnimation(go, go.transform.localPosition, targetPos, 0.2f));
+            }
+            //we should shift back
+            else if (info.isShifted && (info.id < id || id < 0))
+            {
+                info.isShifted = false;
+                Vector3 targetPos = GetLocalCellPosition(info.id);
+                //go.transform.localPosition = GetLocalCellPosition(info.id);
+                StartCoroutine(ShiftAnimation(go, go.transform.localPosition, targetPos, 0.2f));
+            }
+        }
+
+    }
+
+    IEnumerator ShiftAnimation(GameObject go, Vector3 startPos, Vector3 endPos, float animTime)
+    {
+        float elapsed = 0;
+        while (elapsed < animTime)
+        {
+            elapsed += Time.deltaTime;
+            
+            go.transform.localPosition = Vector3.Lerp(startPos, endPos, elapsed/animTime);
+
+            yield return new WaitForEndOfFrame();
+        }
+        yield break;
     }
 }
